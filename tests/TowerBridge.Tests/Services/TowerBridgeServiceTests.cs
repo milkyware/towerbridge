e53fb56838c80995044b88b801d78a25dddc8b97
Cameron Cowen <cameronc94@gmail.com>
2021-10-08 00:23:47 +0100

    Add project files.

---
+++ tests/TowerBridge.Tests/Services/TowerBridgeServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TowerBridge.API.Services;
+
+namespace TowerBridge.API.Services.Tests
+{
+    [TestFixture()]
+    public class TowerBridgeServiceTests
+    {
+        private ITowerBridgeService service;
+
+        [SetUp()]
+        public void Setup()
+        {
+            service = new TowerBridgeService();
+        }
+
+        [Test()]
+        public async Task GetAllTest()
+        {
+            var lifts = await service.GetAllAsync()
+                .ToListAsync();
+            Assert.Greater(lifts.Count(), 0);
+        }
+
+        [Test()]
+        public async Task GetNextAsyncTest()
+        {
+            var lift = await service.GetNextAsync();
+            Assert.NotNull(lift);
+        }
+    }
+}
