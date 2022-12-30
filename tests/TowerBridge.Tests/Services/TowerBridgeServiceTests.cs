0769fb1b76327e5b6fd6013e56286bb6415c4d46
Cameron Cowen <cameronc94@gmail.com>
2022-12-30 13:59:36 +0000

    Additional test tweaks Parameterised html sample resource

--- tests/TowerBridge.Tests/Services/TowerBridgeServiceTests.cs
+++ tests/TowerBridge.Tests/Services/TowerBridgeServiceTests.cs
@@ -22,56 +22,78 @@ namespace TowerBridge.API.Services.Tests
         [SetUp()]
         public void Setup()
         {
+            _dateTimeService = Substitute.For<IDateTimeService>();
             _logger = Substitute.For<ILogger<TowerBridgeService>>();
         }
 
         [Test()]
-        public async Task GetAllTest()
+        [TestCase(nameof(Resources.BridgeLiftsScheduled),
+            5)]
+        public async Task GetAllTest(string sampleResource, int expectedLifts)
         {
             var towerBridgeClient = Substitute.For<ITowerBridgeClient>();
+            var resourceSample = Resources.ResourceManager.GetString(sampleResource);
             var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(Resources.BridgeLiftsScheduled);
+            htmlDoc.LoadHtml(resourceSample);
             towerBridgeClient.GetBridgeLiftsPage()
                 .Returns(Task.FromResult(htmlDoc));
+
             var service = new TowerBridgeService(_dateTimeService, _logger, towerBridgeClient);
             var lifts = await service.GetAllAsync();
-            Assert.Greater(lifts.Count(), 0);
+
+            Assert.NotNull(lifts);
+            Assert.AreEqual(expectedLifts, lifts.Count());
         }
 
         [Test()]
-        public async Task GetNextAsyncTest(DateTime date)
+        [TestCase(nameof(Resources.BridgeLiftsScheduled), 
+            "2023-01-01T00:00:00", 
+            "2023-01-13T18:30:00", 
+            Description = "On 1/1/23 the next lift is 13/1/23 18:30")]
+        [TestCase(nameof(Resources.BridgeLiftsScheduled), 
+            "2023-01-14T00:00:00", 
+            "2023-01-14T22:15:00", 
+            Description = "On 14/1/23 the next lift is 14/1/23 22:15")]
+        public async Task GetNextAsyncTest(string sampleResource, string date, string expectedDate)
         {
-            var dateTimeService = Substitute.For<IDateTimeService>();
-            dateTimeService.GetNow()
-                .Returns(new DateTime(2023, 1, 1));
-
-            var towerBridgeClient = Substitute.For<ITowerBridgeClient>();
-            var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(Resources.BridgeLiftsScheduled);
-            towerBridgeClient.GetBridgeLiftsPage()
-                .Returns(Task.FromResult(htmlDoc));
-
-            var service = new TowerBridgeService(dateTimeService, _logger, towerBridgeClient);
-            var lift = await service.GetNextAsync();
-            Assert.NotNull(lift);
-        }
-
-        [Test()]
-        [TestCase("2023-01-01T00:00:00", 0, TestName = "GetTodayAsyncTest_Lifts0")]
-        [TestCase("2023-01-13T00:00:00", 4, TestName = "GetTodayAsyncTest_Lifts4")]
-        public async Task GetTodayAsyncTest(string date, int expectedLifts)
-        {
-            var dateTimeService = Substitute.For<IDateTimeService>();
-            dateTimeService.GetToday()
+            _dateTimeService.GetNow()
                 .Returns(DateTime.Parse(date));
 
             var towerBridgeClient = Substitute.For<ITowerBridgeClient>();
+            var resourceSample = Resources.ResourceManager.GetString(sampleResource);
             var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(Resources.BridgeLiftsScheduled);
+            htmlDoc.LoadHtml(resourceSample);
             towerBridgeClient.GetBridgeLiftsPage()
                 .Returns(Task.FromResult(htmlDoc));
 
-            var service = new TowerBridgeService(dateTimeService, _logger, towerBridgeClient);
+            var service = new TowerBridgeService(_dateTimeService, _logger, towerBridgeClient);
+            var lift = await service.GetNextAsync();
+            Assert.NotNull(lift);
+            Assert.AreEqual(DateTime.Parse(expectedDate), lift.Date);
+        }
+
+        [Test()]
+        [TestCase(nameof(Resources.BridgeLiftsScheduled), 
+            "2023-01-01T00:00:00", 
+            0, 
+            Description = "Expects 0 bridge lifts for 1/1/23")]
+        [TestCase(nameof(Resources.BridgeLiftsScheduled), 
+            "2023-01-13T00:00:00", 
+            4, 
+            Description = "Expects 4 bridge lifts for 13/1/23")]
+        public async Task GetTodayAsyncTest(string sampleResource, string date, int expectedLifts)
+        {
+            _dateTimeService.GetToday()
+                .Returns(DateTime.Parse(date));
+
+            var towerBridgeClient = Substitute.For<ITowerBridgeClient>();
+            var resourceSample = Resources.ResourceManager.GetString(sampleResource);
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(resourceSample);
+            towerBridgeClient.GetBridgeLiftsPage()
+                .Returns(Task.FromResult(htmlDoc));
+
+            var service = new TowerBridgeService(_dateTimeService, _logger, towerBridgeClient);
             var lifts = await service.GetTodayAsync();
             Assert.NotNull(lifts);
             Assert.AreEqual(expectedLifts, lifts.Count());
