c9f7e494386ff2cb931ee56ba38eb53fff4a4d97
Cameron Cowen <cameronc94@gmail.com>
2022-12-31 20:44:48 +0000

    Fix: Check No Lifts (#2)
    * Added check for no scheduled lifts
    
    * Logging tweaks
    
    * Implemented options based caching expiration
    
    * Fix bug with checking for no lifts
    
    * Added TowerBridgeClient to allow unit testing
    
    * Logging tweaks
    
    * Logging tweaks
    
    * Added test html samples
    
    * Cleanup
    
    * Logging tweaks
    
    * Tweaks
    
    * Added datetimeservice to improve unit testing reliability
    
    * Added global usings to tests
    
    * Fix to GetTodayAsync date logic
    
    * Added tests for GetTodayAsync
    
    * Test package updates
    
    * Added additional lift samples
    
    * Additional test tweaks
    Parameterised html sample resource
    
    * Additional test cases
    
    * Cleanup
    
    * Logging tweaks

---
+++ src/TowerBridge.API/Services/DateTimeService.cs
@@ -0,0 +1,15 @@
+﻿namespace TowerBridge.API.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime GetNow()
+        {
+            return DateTime.Now;
+        }
+
+        public DateTime GetToday()
+        {
+            return DateTime.Today;
+        }
+    }
+}
